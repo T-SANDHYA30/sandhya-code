a67c8610441c64256b24928a7cff9397910f7616
T-SANDHYA30 <thummalurusandhya456@gmail.com>
2024-07-05 10:47:52 +0530

    Java Script File

---
+++ Sidebar.js
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+    return (
+    <div className="sidebabr">
+       <span>
+         Category inforamtion
+         Sidebar
+       </span>
+    </div>
+    );
+    }
+    export default Sidebar;
